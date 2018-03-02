c6937597fb150fdfa7e80506b6d90b6da23bc098
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-03-02 09:27:32 +0100

    cleanup

--- wget.js
+++ wget.js
@@ -6,11 +6,12 @@
  */
 var wget = function (url, callback) {
     var xhr = new XMLHttpRequest();
+    
     xhr.onreadystatechange = function() {
-        if (xhr.readyState == XMLHttpRequest.DONE) {
+        if (xhr.readyState == XMLHttpRequest.DONE)
             callback(xhr.responseText);
-        }
     }
+
     xhr.open('GET', url, true);
     xhr.send(null);  
 };
